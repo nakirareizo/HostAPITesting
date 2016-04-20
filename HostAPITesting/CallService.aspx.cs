4f9356e9fa043585d4c67bae541284ba48a74ab6
appanalyst <appanalyst@NB_MDHQL000056>
2016-04-20 11:32:28 +0800

    Add project files.

---
+++ HostAPITesting/CallService.aspx.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace HostAPITesting
+{
+    public partial class CallService : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            HostAPITesting.HostTrackerAPI.UserService API = new HostTrackerAPI.UserService();
+            API.SignIn("appanalyst@mdec.com.my", "N@jib1607");
+            HostTrackingAPI.HttpTask task = new HostTrackingAPI.HttpTask();
+        }
+    }
+}
